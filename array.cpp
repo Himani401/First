2746c27fdcf45a15de61d57d7519a58e02792855
Himani401 <70468584+Himani401@users.noreply.github.com>
2020-09-15 21:56:04 +0530

    fifth program

---
+++ array.cpp
@@ -0,0 +1,16 @@
+// program to store and display student roll no.in am array
+#include<iostream>
+using namespace std;
+int main()
+{
+	int arr[5],i;
+	for(i=0;i<5;i++)
+	{
+		cout<<"Enter the roll no.\n";
+		cin>>arr[i];
+	}
+	for(i=0;i<5;i++)
+	{
+		cout<<arr[i]<<endl;
+	}
+}
