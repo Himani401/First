2bfb59bd5f1dac25ece3ce614625d4d2a30b1c98
Himani401 <70468584+Himani401@users.noreply.github.com>
2020-09-15 22:08:51 +0530

    sixth

---
+++ subtraction of two numbers.cpp
@@ -0,0 +1,15 @@
+// substraction of two numbers
+#include <iostream>
+using namespace std;
+int main()
+{
+	int firstNumber,secondNumber,subtractionoftwonumbers;
+	cout<<"enter two integers";
+	cin>>firstNumber>>secondNumber;
+	subtractionoftwonumbers = firstNumber - secondNumber;
+	cout<<firstNumber<<"-"<<secondNumber<<"="<<subtractionoftwonumbers;
+	return 0;
+}
+
+	
+
