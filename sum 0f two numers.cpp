349443e03795bd5aa780018804f9ba6643869abc
Himani401 <70468584+Himani401@users.noreply.github.com>
2020-09-15 21:43:26 +0530

    Third program

---
+++ sum 0f two numers.cpp
@@ -0,0 +1,13 @@
+// sum of two numbers
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,sum;
+	cout<<"enter the two numbers\n";
+	cin>>a>>b;
+	cout<<endl<<"Sum of two numbers is:";
+	sum = a+b;
+	cout<<sum;
+	return 0;
+}
